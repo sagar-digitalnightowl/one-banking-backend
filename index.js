3fb06899498d63877f7424b5b5ddfc7683484749
sagar-digitalnightowl <sagar@digitalnightowl.in>
2025-08-11 10:43:25 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ app.listen(port, () => {
   console.log(`${port} : Server start successfully`);
 });
 
-console.log(process.env.ALLOWED_CLIENT, "Allowed origin")
+const allowedOrigins = process.env.ALLOWED_CLIENT.split(",").map((origin) =>
+  origin.trim()
+);
 
-app.use(cors({
-    origin: process.env.ALLOWED_CLIENT,
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 
